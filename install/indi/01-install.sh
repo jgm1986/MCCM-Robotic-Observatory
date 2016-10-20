110a89d876fdcaba6d7ead988863098917b58cc1
Javier Gusano Martinez <jgm1986@hotmail.com>
2016-10-20 12:22:17 +0200

    Included new dependencies.

--- install/indi/01-install.sh
+++ install/indi/01-install.sh
@@ -14,10 +14,12 @@ then
         echo "[ INFO ] Adding INDI repository to Ubuntu."
         sudo apt-add-repository ppa:mutlaqja/ppa
 	sudo apt-get update
+	sudo apt-get install dbus-x11 -y
         echo "[ INFO ] Installing INDI Library including all 3rd party drivers."
         sudo apt-get install indi-full -y
         echo "[ INFO ] Installing latest Ekos."
         sudo apt-get install kstars-bleeding -y
+	echo "[ INFO ] Done!"
 else
     echo "Unsupported Operating System";
 fi
