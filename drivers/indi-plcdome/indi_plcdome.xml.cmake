f3790c0c0afc3dc33f842e4637ead64d31961059
Javier Martinez Gusano <jgm1986@hotmail.com>
2017-07-19 11:27:27 -0700

    Changed driver XML info and renamed XML file.

---
+++ drivers/indi-plcdome/indi_plcdome.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Domes">
+	<device label="PLC Dome">
+                <driver name="PLC Dome">indi_plcdome</driver>
+                <version>@INDI_PLCDOME_VERSION_MAJOR@.@INDI_PLCDOME_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
